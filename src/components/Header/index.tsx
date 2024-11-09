b4e5d5190827f3fa93f91b2841fe8a723ca68e22
Carlos Alexandre <alexfilho062005@gmail.com>
2024-11-08 23:27:28 -0300

    header styles

--- src/components/Header/index.tsx
+++ src/components/Header/index.tsx
@@ -1,10 +1,15 @@
 import { MapPin } from 'phosphor-react';
+import { Cart } from '../ui/Cart';
+import { NavLink } from 'react-router-dom';
 
 export function Header() {
   return (
-    <header className="bg-background py-8">
-      <div className="flex items-center justify-between max-w-[1160px] mx-auto">
-        <img src="Logo.svg" />
+    <header className="bg-background py-8 sticky top-0 z-10">
+      <div className="flex items-center justify-between max-w-[72.5rem] xl:mx-auto mx-3">
+        <NavLink to="/">
+          <img src="Logo.svg" />
+        </NavLink>
+
         <div className="flex gap-3 items-center">
           <div className="flex gap-1 items-center bg-purple-light rounded-md p-2">
             <MapPin className="size-[22px] text-purple" weight="fill" />
@@ -12,6 +17,7 @@ export function Header() {
               Fortaleza, CE
             </span>
           </div>
+          <Cart />
         </div>
       </div>
     </header>
--- src/components/ui/Cart.tsx
+++ src/components/ui/Cart.tsx
@@ -1,3 +1,16 @@
+import { ShoppingCart } from 'phosphor-react';
+import { Link } from 'react-router-dom';
+
 export function Cart() {
-  return <div></div>;
+  return (
+    <Link
+      to="/checkout"
+      className="p-2 text-yellow-dark bg-yellow-light rounded-md cursor-pointer relative"
+    >
+      <ShoppingCart className="size-[20px]" weight="fill" />
+      <span className="flex items-center justify-center absolute bg-yellow-dark size-[20px] rounded-full -top-2 -right-2 text-white text-xs font-roboto font-bold">
+        3
+      </span>
+    </Link>
+  );
 }
