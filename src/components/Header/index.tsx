60075c066451ac46a5ea4d227b6e1972404e1da9
Carlos Alexandre <alexfilho062005@gmail.com>
2024-11-08 21:50:22 -0300

    feat: header base

--- src/components/Header/index.tsx
+++ src/components/Header/index.tsx
@@ -2,8 +2,18 @@ import { MapPin } from 'phosphor-react';
 
 export function Header() {
   return (
-    <div>
-      <MapPin className="w-24 h-24 text-purple" weight="fill" />
-    </div>
+    <header className="bg-background py-8">
+      <div className="flex items-center justify-between max-w-[1160px] mx-auto">
+        <img src="Logo.svg" />
+        <div className="flex gap-3 items-center">
+          <div className="flex gap-1 items-center bg-purple-light rounded-md p-2">
+            <MapPin className="size-[22px] text-purple" weight="fill" />
+            <span className="text-purle-dark font-roboto text-sm">
+              Fortaleza, CE
+            </span>
+          </div>
+        </div>
+      </div>
+    </header>
   );
 }
