117b25fbd6e98feac668f63098db7e5a955524b9
Carlos Alexandre <alexfilho062005@gmail.com>
2024-11-13 12:18:48 -0300

    adjustments

--- src/layouts/DefaultLayout.tsx
+++ src/layouts/DefaultLayout.tsx
@@ -3,7 +3,7 @@ import { Header } from '../components/Header';
 
 export function DefaultLayout() {
   return (
-    <div className="bg-background">
+    <div className="bg-background dark:bg-zinc-900 h-screen">
       <Header />
       <Outlet />
     </div>
--- src/pages/Home/components/coffee-list/index.tsx
+++ src/pages/Home/components/coffee-list/index.tsx
@@ -4,7 +4,7 @@ export function CoffeeList() {
   return (
     <section className="dark:bg-zinc-900 bg-background">
       <div className="max-w-container mx-auto xl:px-0 px-3">
-        <h2 className="font-baloo font-extrabold leading-line text-[32px] text-base-subtitle mb-[54px] dark:text-zinc-100">
+        <h2 className="font-baloo font-extrabold leading-line text-[32px] text-base-subtitle pt-2 mb-[54px] dark:text-zinc-100">
           Nossos cafés
         </h2>
         <div className="flex justify-center xl:block">
