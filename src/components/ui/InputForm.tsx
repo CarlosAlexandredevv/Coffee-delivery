c0a7d4f6b5f654894d42c07dab934c33ef0f22c4
Carlos Alexandre <alexfilho062005@gmail.com>
2024-12-09 09:10:32 -0300

    feat:input form styles

---
+++ src/components/ui/InputForm.tsx
@@ -0,0 +1,19 @@
+interface InputFormProps extends React.InputHTMLAttributes<HTMLInputElement> {
+  opcional?: boolean;
+}
+
+export function InputForm({ opcional = false, ...props }: InputFormProps) {
+  return (
+    <div className="relative flex items-center justify-between rounded border border-base-button bg-base-input p-3 focus-within:border-yellow-dark">
+      <input
+        className="w-full rounded bg-transparent font-roboto text-sm leading-line text-base-text outline-none placeholder:text-base-label focus:placeholder:text-transparent"
+        {...props}
+      />
+      {opcional && (
+        <span className="font-roboto text-xs italic leading-line text-base-label">
+          Opcional
+        </span>
+      )}
+    </div>
+  );
+}
--- src/pages/Checkout/index.tsx
+++ src/pages/Checkout/index.tsx
@@ -1,3 +1,16 @@
+import { InputForm } from "@/components/ui/InputForm";
+
 export function Checkout() {
-  return <div>Checkout</div>;
+  return (
+    <main className="px-3">
+      <div className="mx-auto max-w-container">
+        <h1 className="font-baloo text-lg font-bold leading-line text-base-subtitle dark:text-zinc-100">
+          Complete seu pedido
+        </h1>
+        <div>
+          <InputForm placeholder="Label" opcional />
+        </div>
+      </div>
+    </main>
+  );
 }
